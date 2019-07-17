ada3dd151602b2a361c60105b192df9134e5ece9
Evan Anders <evanhanders@gmail.com>
2019-07-17 15:59:25 -0600

    incorporates Daniel's suggestions; does not yet put the flux discussion in (need to figure out how to handle PE + KE in addition to F_enth. Or do we ignore them?)

--- code/plotting/thermal_sun_comparison.py
+++ code/plotting/thermal_sun_comparison.py
@@ -39,8 +39,10 @@ def theory_dT_dt(t, B0, Gamma0, f, chi, beta, t_off, T0, grad_T_ad=-1, m_ad=1.5)
     return C0*this_T**(m_ad/2)/np.sqrt(t + t_off)
 
 def read_solar_profiles():
-    mesa_data = np.genfromtxt('sun_4.5gyr.data', skip_header=6, usecols=(2, 4, 9)) #logR (in Rsun units), logRho (g/cm^3), csound (cm/s)
-    csound = mesa_data[:,2]*cm2Mm
+    mesa_data = np.genfromtxt('sun_4.5gyr.data', skip_header=6, usecols=(2, 4, 9, 10, 11)) #logR (in Rsun units), logRho (g/cm^3), grav (cm/s^2), Cp, csound (cm/s)
+    grav = mesa_data[:,2]
+    Cp = mesa_data[:,3] #erg/K?
+    csound = mesa_data[:,4]*cm2Mm
     mesa_data = 10**(mesa_data[:,:-1])
     radius    = mesa_data[:,0]*Rsun2Mm
     density    = mesa_data[:,1]*(cm2Mm)**(-3)
@@ -67,13 +69,17 @@ def read_solar_profiles():
     radii *= Rsun2Mm
 
     rho2r = interp1d(density, radius, bounds_error=False, fill_value='extrapolate')
-    r2cs = interp1d(radius, csound, bounds_error=False, fill_value='extrapolate')
     r2chi = interp1d(radii, chi, bounds_error=False, fill_value='extrapolate')
     r2nu  = interp1d(radii, nu, bounds_error=False, fill_value='extrapolate')
 
-    return rho2r, r2chi, r2nu, r2cs
 
-rho2r, r2chi, r2nu, r2cs = read_solar_profiles()
+    r2cs = interp1d(radius, csound, bounds_error=False, fill_value='extrapolate')
+    r2gDivCp = interp1d(radius, grav/Cp, bounds_error=False, fill_value='extrapolate')
+    r2Cp = interp1d(radius, Cp, bounds_error=False, fill_value='extrapolate')
+
+    return rho2r, r2chi, r2nu, r2cs, r2gDivCp, r2Cp
+
+rho2r, r2chi, r2nu, r2cs, r2gDivCp, r2Cp = read_solar_profiles()
 
 
 
@@ -94,6 +100,7 @@ epsilon     = np.abs(S1_over_cp)
 #Other parameters
 L           = 0.1        #Mm
 g           = 2.74e-4    #Mm/s^2
+#Need Cp. Sadness.
 cs          = 9.5e-3     #Mm/s, AL08
 u_th        = np.sqrt(epsilon)*cs
 
@@ -102,7 +109,7 @@ R_approx    = cs**2/T0
 
 
 B       = (4.*np.pi/3) * g * (L/2)**3 * S1_over_cp * rho_mean
-nondim  = rho_top * L**2 * u_th**2 * np.abs(S1_over_cp)
+nondim  = rho_top * L**2 * u_th**2 * np.abs(S1_over_cp) #g * Mm / s^2
 
 print('dimensional B: {:.4e}, nondim B: {:.4e}'.format(B, B/nondim))
 
@@ -124,6 +131,7 @@ rho_outs = []
 r_outs   = []
 T_outs   = []
 w_outs   = []
+fits_outs = []
 
 B_nondim = B/nondim
 
@@ -141,11 +149,11 @@ for Buoy in (B_nondim/2, 2*B_nondim):
     if t_off < 0:
         t_off = 0
     grad_T_ad = -(np.exp(n_rho/m_ad)-1)/20
-    T0 = 1 + grad_T_ad*(z0-20)
+    Tatm = 1 + grad_T_ad*(z0-20)
 
-    Temps = theory_T(times, B0, Gamma, m, chi, k, t_off, T0, grad_T_ad=grad_T_ad, m_ad=m_ad)
+    Temps = theory_T(times, B0, Gamma, m, chi, k, t_off, Tatm, grad_T_ad=grad_T_ad, m_ad=m_ad)
     Rhos  = Temps**(m_ad)
-    ws    = theory_dT_dt(times, B0, Gamma, m, chi, k, t_off, T0, grad_T_ad=grad_T_ad, m_ad=m_ad)/grad_T_ad
+    ws    = theory_dT_dt(times, B0, Gamma, m, chi, k, t_off, Tatm, grad_T_ad=grad_T_ad, m_ad=m_ad)/grad_T_ad
 
     rho_f = interp1d(times, Rhos)
     rs    = theory_r(times, B0, Gamma, chi, t_off, rho_f=rho_f)
@@ -154,10 +162,11 @@ for Buoy in (B_nondim/2, 2*B_nondim):
     T_outs.append(Temps)
     w_outs.append(ws)
     r_outs.append(rs)
+    fits_outs.append(dataset)
 
 
 fig = plt.figure(figsize=(3.25, 6))
-ax = fig.add_subplot(3,1,1)
+ax = fig.add_subplot(4,1,1)
 axs = [ax]
 
 #Plot radius grey band
@@ -168,9 +177,13 @@ plt.fill_between(rho2r(x_values)/(Rsun2Mm), lower_y_values, upper_y_values, colo
 
 #plot actual radius measurements
 solar_radii = []
+solar_therm_radii = []
+solar_rhos = []
 for i in range(2):
-    solar_radii.append(rho2r(rho_outs[i]*(rho_top/rho_outs[i][0])))
-    plt.plot(solar_radii[i]/(Rsun2Mm), r_outs[i]*(L/2/r_outs[i][0]), c='k', lw=0.5*(i+1))
+    solar_rhos.append(rho_outs[i]*(rho_top/rho_outs[i][0]))
+    solar_radii.append(rho2r(solar_rhos[i]))
+    solar_therm_radii.append(r_outs[i]*(L/2/r_outs[i][0]))
+    plt.plot(solar_radii[i]/(Rsun2Mm), solar_therm_radii[i], c='k', lw=0.5*(i+1))
 
 plt.plot(solar_radii[0]/(Rsun2Mm), (L/2)/(rho_outs[0]/rho_outs[0][0])**(1./2), c='k', lw=2)
 ax.text(0.69, 8e-5, r'$r \propto \rho^{-1/2}$', rotation=3)
@@ -182,7 +195,7 @@ plt.yscale('log')
 plt.ylim(1e-5, 1e-1)
 plt.ylabel(r'$r_{\mathrm{th}}$ (Mm)')
 
-ax = fig.add_subplot(3,1,2)
+ax = fig.add_subplot(4,1,2)
 axs += [ax]
 #Plot radius grey band
 x_values = solar_radii[0] 
@@ -191,8 +204,10 @@ lower_y_values = interp1d(solar_radii[1], w_outs[1]*(u_th/w_outs[1][0]))(x_value
 plt.fill_between(x_values/(Rsun2Mm), lower_y_values, upper_y_values, color='black', alpha=0.4, rasterized=True)
 
 #plot actual velocity measurements
+solar_therm_w = []
 for i in range(2):
-    plt.plot(solar_radii[i]/(Rsun2Mm), w_outs[i]*(u_th/w_outs[i][0]), c='k', lw=0.5*(i+1))
+    solar_therm_w.append(w_outs[i]*(u_th/w_outs[i][0]))
+    plt.plot(solar_radii[i]/(Rsun2Mm), solar_therm_w[i], c='k', lw=0.5*(i+1))
 plt.plot(solar_radii[i]/Rsun2Mm, r2cs(solar_radii[i]), c='k', lw=1, dashes=(5,1,2,1))
 ax.text(0.69, 3e-1, r'$c_s$', rotation=-3)
 
@@ -203,14 +218,14 @@ plt.yscale('log')
 plt.ylabel(r'$w_{\mathrm{th}}$ (Mm/s)')
 
 
-ax = fig.add_subplot(3,1,3)
+ax = fig.add_subplot(4,1,3)
 axs += [ax]
 
 #Plot grey band
 tau_ratios = []
 for i in range(2):
     chi    = r2chi(solar_radii[i])
-    nu     = r2chi(solar_radii[i])
+    nu     = r2nu(solar_radii[i])
 
     tau_kappa = r_outs[i]*(L/2/r_outs[i][0])**2/chi
     tau_ff    = -r_outs[i]/(w_outs[i]*u_th)
@@ -240,14 +255,35 @@ plt.xlabel(r'radius $(R_{\odot})$')
 #plt.legend(loc='best', fontsize=9, frameon=False)
 axs[2].set_ylim(3e5, 3e8)
 
-#for i, bounds in enumerate(((3e-2, 1e-1, 0.66, 0.705), (4.8e-2, 1e-1, 0.66, 0.705), (1e8, 4e8, 0.66, 0.705))):
-#    bot, top, left, right = bounds
-#    axs[i].plot((left, right), (bot, bot), c='k', lw=1)#0.75)
-#    axs[i].plot((right, right), (bot, top), c='k', lw=1)#0.75)
-#
-#axs[0].text(0.685, 4e-2, 'a',   fontsize=15)#, weight='bold')
-#axs[1].text(0.684, 5.5e-2, 'b', fontsize=15)#, weight='bold')
-#axs[2].text(0.685, 1.35e8, 'c', fontsize=15)#, weight='bold')
+
+ax = fig.add_subplot(4,1,4)
+
+for i in range(2):
+    solar_T = T_outs[i]*(T0/T_outs[i][0])
+    V = fits_outs[i][-3]*(solar_therm_radii[i]/cm2Mm)**3
+    rho = solar_rhos[i]*cm2Mm**3
+    w = -solar_therm_w[i]/cm2Mm
+    cross_area = np.pi*solar_therm_radii[i]**2/cm2Mm**2
+    g_over_Cp = r2gDivCp(solar_radii[i])
+    B = (fits_outs[i][3]*nondim/cm2Mm)
+    sfluc = B / (rho*V*g_over_Cp)
+
+    F_ke = 0.5*rho*w**3*cross_area
+    F_enth = rho*solar_T*sfluc*w*cross_area
+
+
+    plt.axhline(3.84e33, c='k')
+    plt.plot(solar_radii[i]/(Rsun2Mm), F_enth, c='k', lw=0.5*(i+1))
+    plt.plot(solar_radii[i]/(Rsun2Mm), -F_ke, c='b', lw=0.5*(i+1))
+    for r, rth, ln_rho, Fe, Fk in zip(solar_radii[i]/Rsun2Mm, solar_therm_radii[i]/Rsun2Mm, np.log(solar_rhos[i]/rho_top), F_enth, F_ke):
+        if r > 0.68:         print(r, rth, ln_rho, Fe+Fk)
+#    plt.plot(solar_radii[i]/(Rsun2Mm), -(F_ke), c='b', lw=0.5*(i+1))
+
+plt.xlim(0.68, 0.995)
+plt.yscale('log')
+plt.ylabel(r'Flux')
+plt.xlabel(r'radius $(R_{\odot})$')
+
 
 
 plt.savefig('thermal_sun_comparison.png', dpi=300, bbox_inches='tight')
